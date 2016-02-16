b5cd15d5616a68212736de6c36c4c635db446a00
Khaled Hosny <khaledhosny@eglug.org>
2016-02-16 13:59:53 +0400

    Add API to get the FT_Face from the ScFace

--- scribus/fonts/scface.h
+++ scribus/fonts/scface.h
@@ -25,6 +25,9 @@ virtual:      dispatch to constituents, handle embedding (-)
 #include <QString>
 #include <utility>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include "fpointarray.h"
 
 
@@ -185,6 +188,7 @@ public:
 		virtual GlyphMetrics glyphBBox(gid_type gl, qreal sz) const;
 		virtual bool EmbedFont(QByteArray &/*str*/)       const { return false; }
 		virtual void RawData(QByteArray & /*bb*/)      const {}
+		virtual FT_Face ftFace() const { return 0; }
 
 		virtual bool isCIDKeyed() const { return isCIDFont; }
 		virtual bool hasNames() const { return hasGlyphNames; }
@@ -272,6 +276,9 @@ public:
 
 	/// if the fontfile contains more than one face, the index, else -1
 	int faceIndex()    const { return m_m->faceIndex; }
+
+	/// the FreeType face of this font
+	FT_Face ftFace() const { return m_m->ftFace(); }
 
 	/// path name of the document this face is local to
 	QString localForDocument()  const { return m_m->forDocument; }
