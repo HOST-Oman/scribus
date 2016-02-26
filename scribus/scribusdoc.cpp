6b0f4bbcee13389104491252f9777ef0673b73a4
Khaled Hosny <khaledhosny@eglug.org>
2016-02-26 13:53:12 +0400

    Minor

--- scribus/scribusdoc.cpp
+++ scribus/scribusdoc.cpp
@@ -4507,14 +4507,7 @@ public:
 		: m_really(Really)
 	{}
 
-	void drawGlyph(GlyphLayout gl) { collectGlyph(gl); }
-	void drawGlyphOutline(GlyphLayout gl) { collectGlyph(gl); }
-
-	// we don't need this one
-	void drawLine(QPointF, QPointF) {}
-
-private:
-	void collectGlyph(GlyphLayout gl)
+	void drawGlyph(GlyphLayout gl)
 	{
 		if (!font().replacementName().isEmpty())
 		{
@@ -4523,6 +4516,12 @@ private:
 		}
 	}
 
+	void drawGlyphOutline(GlyphLayout gl) { drawGlyph(gl); }
+
+	// we don't need this one
+	void drawLine(QPointF, QPointF) {}
+
+private:
 	QMap<QString, QMap<uint, FPointArray> > & m_really;
 };
 
