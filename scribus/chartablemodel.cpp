andreas-vox/ScribusCTL

Starting to implement general painter.
I am trying to implement specific painter for each class that need to
render boxes for example screen painter and pdf painter and all these
clases will inherant from a general painter called ScPainter

scribus/chartablemodel.cpp
@@ -81,7 +81,7 @@ QVariant CharTableModel::data(const QModelIndex &index, int role) const
 		double ww = baseSize - face.glyphWidth(gl, baseSize);
 
 		QImage pix(baseSize, size, QImage::Format_ARGB32_Premultiplied);
-		ScPainter *p = new ScPainter(&pix, baseSize, size);
+		ScreenPainter *p = new ScreenPainter(&pix, baseSize, size);
 		p->clear();
 		FPointArray gly = face.glyphOutline(gl, 1);
 		if (gly.size() > 4)

scribus/pdflib_core.h
@@ -42,6 +42,13 @@ class ScText;
 #endif
 
 #include "pdfwriter.h"
+class PDFPainter : public ScreenPainter
+{
+    virtual void drawGlyph(GlyphRun run);
+    virtual void translate();
+    virtual void restore();
+    virtual void save();
+};
 
 /**
  * PDFLibCore provides Scribus's implementation of PDF export functionality.

scribus/scribusview.cpp
@@ -2634,7 +2634,7 @@ QImage ScribusView::MPageToPixmap(QString name, int maxGr, bool drawFrame)
 		m_canvas->setPreviewMode(true);
 		m_canvas->setForcedRedraw(true);
 		pm = QImage(clipw, cliph, QImage::Format_ARGB32_Premultiplied);
-		ScPainter *painter = new ScPainter(&pm, pm.width(), pm.height(), 1.0, 0);
+		ScreenPainter *painter = new ScreenPainter(&pm, pm.width(), pm.height(), 1.0, 0);
 		painter->clear(Doc->paperColor());
 		painter->translate(-clipx, -clipy);
 		painter->setLineWidth(1);
@@ -2715,11 +2715,11 @@ QImage ScribusView::PageToPixmap(int Nr, int maxGr, bool drawFrame, bool drawBac
 			Doc->setMasterPageMode(false);
 			Doc->setLoading(true);
 			Doc->setCurrentPage(Doc->DocPages.at(Nr));
-			ScPainter *painter = new ScPainter(&im, im.width(), im.height(), 1.0, 0);
+			ScreenPainter *painter = new ScreenPainter(&im, im.width(), im.height(), 1.0, 0);
 			if (drawBackground)
 				painter->clear(Doc->paperColor());
 			painter->translate(-clipx, -clipy);
-			painter->setFillMode(ScPainter::Solid);
+			painter->setFillMode(ScreenPainter::Solid);
 			if (drawFrame)
 			{
 				painter->setPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);

scribus/ui/autoformbuttongroup.cpp
@@ -744,11 +744,11 @@ QPixmap AutoformButtonGroup::getIconPixmap(int nr, int pixmapSize)
 		Path.addPoint(x1, y1);
 		Path.addPoint(x2, y2);
 	}
-	ScPainter *painter = new ScPainter(&Ico, 32, 32);
+	ScreenPainter *painter = new ScreenPainter(&Ico, 32, 32);
 	painter->setBrush(qRgb(238, 238, 236));
 	painter->setPen(qRgb(137, 139, 134), 2.0, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);
-	painter->setFillMode(ScPainter::Solid);
-	painter->setStrokeMode(ScPainter::Solid);
+	painter->setFillMode(ScreenPainter::Solid);
+	painter->setStrokeMode(ScreenPainter::Solid);
 	painter->translate(2.0, 2.0);
 	painter->setupPolygon(&Path);
 	painter->drawPolygon();

scribus/ui/gradientpreview.cpp
@@ -85,7 +85,7 @@ void GradientPreview::paintEvent(QPaintEvent *e)
 	pb.begin(&pixm);
 	pb.fillRect(0, 0, pixm.width(), pixm.height(), b);
 	pb.end();
-	ScPainter *p = new ScPainter(&pixm, width()-20, 37);
+	ScreenPainter *p = new ScreenPainter(&pixm, width()-20, 37);
 //	p->clear(Qt::white);
 	p->setPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);
 	p->setFillMode(2);

HOST-Oman/scribus

scribus/chartablemodel.cpp
@@ -81,7 +81,7 @@ QVariant CharTableModel::data(const QModelIndex &index, int role) const
 		double ww = baseSize - face.glyphWidth(gl, baseSize);
 
 		QImage pix(baseSize, size, QImage::Format_ARGB32_Premultiplied);
-		ScPainter *p = new ScPainter(&pix, baseSize, size);
+		ScPainter *p = new ScScreenPainter(&pix, baseSize, size);
 		p->clear();
 		FPointArray gly = face.glyphOutline(gl, 1);
 		if (gly.size() > 4)

scribus/pdflib_core.h
@@ -42,6 +42,13 @@ class ScText;
 #endif
 
 #include "pdfwriter.h"
+class PDFPainter : public ScPainter
+{
+    virtual void drawGlyph(GlyphRun run);
+    virtual void translate();
+    virtual void restore();
+    virtual void save();
+};
 
 /**
  * PDFLibCore provides Scribus's implementation of PDF export functionality.

scribus/scribusview.cpp
@@ -2641,7 +2641,7 @@ QImage ScribusView::MPageToPixmap(QString name, int maxGr, bool drawFrame)
 		m_canvas->setPreviewMode(true);
 		m_canvas->setForcedRedraw(true);
 		pm = QImage(clipw, cliph, QImage::Format_ARGB32_Premultiplied);
-		ScPainter *painter = new ScPainter(&pm, pm.width(), pm.height(), 1.0, 0);
+		ScPainter *painter = new ScScreenPainter(&pm, pm.width(), pm.height(), 1.0, 0);
 		painter->clear(Doc->paperColor());
 		painter->translate(-clipx, -clipy);
 		painter->setLineWidth(1);
@@ -2724,7 +2724,7 @@ QImage ScribusView::PageToPixmap(int Nr, int maxGr, bool drawFrame, bool drawBac
 	Doc->setMasterPageMode(false);
 	Doc->setLoading(true);
 	Doc->setCurrentPage(Doc->DocPages.at(Nr));
-	ScPainter *painter = new ScPainter(&im, im.width(), im.height(), 1.0, 0);
+	ScPainter *painter = new ScScreenPainter(&im, im.width(), im.height(), 1.0, 0);
 	if (drawBackground)
 		painter->clear(Doc->paperColor());
 	painter->translate(-clipx, -clipy);

scribus/ui/autoformbuttongroup.cpp
@@ -744,7 +744,7 @@ QPixmap AutoformButtonGroup::getIconPixmap(int nr, int pixmapSize)
 		Path.addPoint(x1, y1);
 		Path.addPoint(x2, y2);
 	}
-	ScPainter *painter = new ScPainter(&Ico, 32, 32);
+	ScPainter *painter = new ScScreenPainter(&Ico, 32, 32);
 	painter->setBrush(qRgb(238, 238, 236));
 	painter->setPen(qRgb(137, 139, 134), 2.0, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);
 	painter->setFillMode(ScPainter::Solid);

scribus/ui/gradientpreview.cpp
@@ -86,7 +86,7 @@ void GradientPreview::paintEvent(QPaintEvent *e)
 	pb.begin(&pixm);
 	pb.fillRect(0, 0, pixm.width(), pixm.height(), b);
 	pb.end();
-	ScPainter *p = new ScPainter(&pixm, width()-20, 37);
+	ScPainter *p = new ScScreenPainter(&pixm, width()-20, 37);
 //	p->clear(Qt::white);
 	p->setPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);
 	p->setFillMode(2);