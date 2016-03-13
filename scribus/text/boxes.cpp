andreas-vox/ScribusCTL

Fix small caps problems

scribus/text/boxes.cpp
@@ -130,6 +130,17 @@ LineBox::LineBox()
 	m_type = T_Line;
 }
 
+void LineBox::render(ScreenPainter *p, const StoryText &text)
+{
+	p->save();
+	p->translate(x(),y() + ascent());
+	for (int i = 0; i < boxes().count(); i++)
+	{
+		Box* box = boxes()[i];
+		box->render(p, text);
+	}
+	p->restore();
+}
 //void LineBox::justify(const ParagraphStyle& style)
 //{
 //	double glyphNatural = 0;
@@ -310,7 +321,7 @@ void GlyphBox::render(ScreenPainter *p, const StoryText &text)
 			}
 			else
 			{
-				st = font.underlinePos(style.fontSize());
+				st = font.underlinePos(style.fontSize() / 10.0);
 				lw = qMax(font.strokeWidth(style.fontSize() / 10.0), 1.0);
 			}
 			if (style.baselineOffset() != 0)

scribus/text/boxes.h
@@ -142,7 +142,10 @@ public:
 	GlyphRun glyphs;
 	QList <GlyphLayout> m_glyphs;
 	int pointToPosition(FPoint coord) const;
-	FRect boundingBox(int pos, uint len = 1) const { return bbox(); }
+	FRect boundingBox(int pos, uint len = 1) const
+	{
+		return bbox();
+	}
 //	QList<const Box*> pathForPos(int pos) const;
 	void render(ScreenPainter* p, const StoryText& text);
 	void setQColor(QColor *tmp, QString colorName, double shad);
@@ -153,6 +156,7 @@ class LineBox : public GroupBox
 {
 public:	
 	LineBox();
+	void render(ScreenPainter* p, const StoryText& text);
 //	void justify(const ParagraphStyle& style);
 	qreal colLeft;
 };

HOST-Oman/scribus

scribus/text/boxes.cpp
@@ -130,6 +130,17 @@ LineBox::LineBox()
 	m_type = T_Line;
 }
 
+void LineBox::render(ScPainter *p, const StoryText &text)
+{
+	p->save();
+	p->translate(x(),y() + ascent());
+	for (int i = 0; i < boxes().count(); i++)
+	{
+		Box* box = boxes()[i];
+		box->render(p, text);
+	}
+	p->restore();
+}
 //void LineBox::justify(const ParagraphStyle& style)
 //{
 //	double glyphNatural = 0;
@@ -310,7 +321,7 @@ void GlyphBox::render(ScPainter *p, const StoryText &text)
 			}
 			else
 			{
-				st = font.underlinePos(style.fontSize());
+				st = font.underlinePos(style.fontSize() / 10.0);
 				lw = qMax(font.strokeWidth(style.fontSize() / 10.0), 1.0);
 			}
 			if (style.baselineOffset() != 0)

scribus/text/boxes.h
@@ -143,7 +143,10 @@ public:
 	GlyphRun glyphs;
 	QList <GlyphLayout> m_glyphs;
 	int pointToPosition(FPoint coord) const;
-	FRect boundingBox(int pos, uint len = 1) const { return bbox(); }
+	FRect boundingBox(int pos, uint len = 1) const
+	{
+		return bbox();
+	}
 //	QList<const Box*> pathForPos(int pos) const;
 	void render(ScPainter* p, const StoryText& text);
 	void setQColor(QColor *tmp, QString colorName, double shad);
@@ -154,6 +157,7 @@ class LineBox : public GroupBox
 {
 public:	
 	LineBox();
+	void render(ScPainter* p, const StoryText& text);
 //	void justify(const ParagraphStyle& style);
 	qreal colLeft;
 };