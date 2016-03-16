dfffe2e5a6291b8e3faf20b40ac074cfb6bdbb29
shamsa <shamsahamed59@gmail.com>
2016-03-16 11:23:50 +0400

    Fix left-aligned of last line of fully justified RTL paragraph. Close #28.

--- scribus/pageitem_textframe.cpp
+++ scribus/pageitem_textframe.cpp
@@ -3156,7 +3156,13 @@ void PageItem_TextFrame::layout()
 			if (style.alignment() == ParagraphStyle::Centered)
 				OFs = (current.line.width - current.line.naturalWidth) / 2;
 			if (style.alignment() == ParagraphStyle::Justified)
-				OFs = 0;
+			{
+				if (style.direction() == ParagraphStyle::RTL)
+					OFs = current.line.width - current.line.naturalWidth;
+				else
+					OFs = 0;
+			}
+
 			if (style.alignment() == ParagraphStyle::Extended
 				|| (style.alignment() == ParagraphStyle::Justified
 					&&  (itemText.text(a) == SpecialChars::LINEBREAK ||
