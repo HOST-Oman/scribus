744c5aab8dce3fcd2b96547f0ad7316b2e8e9d20
Khaled Hosny <khaledhosny@eglug.org>
2016-03-16 21:36:15 +0400

    Indent with tabs

--- scribus/text/textlayoutpainter.h
+++ scribus/text/textlayoutpainter.h
@@ -16,83 +16,83 @@
 
 struct TextLayoutColor
 {
-        QString color;
-        double shade;
+	QString color;
+	double shade;
 
-        TextLayoutColor()
-                : color("Black")
-                , shade(100)
-        { }
+	TextLayoutColor()
+		: color("Black")
+		, shade(100)
+	{ }
 
-        TextLayoutColor(QString c, double s=100)
-                : color(c)
-                , shade(s)
-        { }
+	TextLayoutColor(QString c, double s=100)
+		: color(c)
+		, shade(s)
+	{ }
 };
 
 class SCRIBUS_API TextLayoutPainter
 {
 public:
-        TextLayoutPainter() { }
-        virtual ~TextLayoutPainter();
+	TextLayoutPainter() { }
+	virtual ~TextLayoutPainter();
 
-        virtual void setFont(const ScFace font);
-        virtual ScFace font();
+	virtual void setFont(const ScFace font);
+	virtual ScFace font();
 
-        virtual void setFontSize(double size);
-        virtual double fontSize();
+	virtual void setFontSize(double size);
+	virtual double fontSize();
 
-        virtual void setStrokeColor(TextLayoutColor c);
-        virtual TextLayoutColor strokeColor();
+	virtual void setStrokeColor(TextLayoutColor c);
+	virtual TextLayoutColor strokeColor();
 
-        virtual void setFillColor(TextLayoutColor c);
-        virtual TextLayoutColor fillColor();
+	virtual void setFillColor(TextLayoutColor c);
+	virtual TextLayoutColor fillColor();
 
-        virtual void setStrokeWidth(double w);
-        virtual double strokeWidth();
+	virtual void setStrokeWidth(double w);
+	virtual double strokeWidth();
 
-        virtual void translate(double x, double y);
-        virtual double x();
-        virtual double y();
+	virtual void translate(double x, double y);
+	virtual double x();
+	virtual double y();
 
-        virtual void scale(double h, double v);
-        virtual double getScaleV();
-        virtual double getScaleH();
+	virtual void scale(double h, double v);
+	virtual double getScaleV();
+	virtual double getScaleH();
 
-        virtual void drawGlyph(const GlyphLayout gl, bool selected) = 0;
-        virtual void drawGlyphOutline(const GlyphLayout gl, bool fill, bool selected) = 0;
-        virtual void drawLine(QPointF start, QPointF end) = 0;
-        virtual void drawRect(QRectF rect) = 0;
-        virtual void drawObject(PageItem* item) = 0;
+	virtual void drawGlyph(const GlyphLayout gl, bool selected) = 0;
+	virtual void drawGlyphOutline(const GlyphLayout gl, bool fill, bool selected) = 0;
+	virtual void drawLine(QPointF start, QPointF end) = 0;
+	virtual void drawRect(QRectF rect) = 0;
+	virtual void drawObject(PageItem* item) = 0;
 
-        virtual void save();
-        virtual void restore();
+	virtual void save();
+	virtual void restore();
 
 private:
-        struct State
-        {
-                ScFace font;
-                double fontSize;
-                TextLayoutColor strokeColor;
-                TextLayoutColor fillColor;
-                double strokeWidth;
-                double x;
-                double y;
-                double scaleH;
-                double scaleV;
+	struct State
+	{
+		ScFace font;
+		double fontSize;
+		TextLayoutColor strokeColor;
+		TextLayoutColor fillColor;
+		double strokeWidth;
+		double x;
+		double y;
+		double scaleH;
+		double scaleV;
 
-                State()
-                        : fontSize(0)
-                        , strokeWidth(0)
-                        , x(0)
-                        , y(0)
-                        , scaleH(1)
-                        , scaleV(1)
-                {}
-        };
+		State()
+			: fontSize(0)
+			, strokeWidth(0)
+			, x(0)
+			, y(0)
+			, scaleH(1)
+			, scaleV(1)
+		{}
+	};
 
-        State m_state;
-        QStack<State> m_stack;
+	State m_state;
+	QStack<State> m_stack;
 };
 
 #endif // TEXTLAYOUTPAINTER_H
