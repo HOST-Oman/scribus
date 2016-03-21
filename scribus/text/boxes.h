a6885708a53083bce7666a4ac6ffc233e9f293d6
Khaled Hosny <khaledhosny@eglug.org>
2016-03-22 00:33:10 +0400

    Add license headers

--- scribus/text/boxes.h
+++ scribus/text/boxes.h
@@ -1,10 +1,9 @@
-//
-//  boxes.h
-//  Scribus
-//
-//  Created by Andreas Vox on 23.05.15.
-//
-//
+/*
+ For general Scribus (>=1.3.2) copyright and licensing information please refer
+ to the COPYING file provided with the program. Following this notice may exist
+ a copyright and/or license notice that predates the release of Scribus 1.3.2
+ for which a new license (GPL+exception) is in place.
+ */
 
 #ifndef __Scribus__boxes__
 #define __Scribus__boxes__
