andreas-vox/ScribusCTL

Starting to implement general painter.
I am trying to implement specific painter for each class that need to
render boxes for example screen painter and pdf painter and all these
clases will inherant from a general painter called ScPainter

scribus/scribusview.cpp
@@ -2634,7 +2634,7 @@ QImage ScribusView::MPageToPixmap(QString name, int maxGr, bool drawFrame)
 		m_canvas->setPreviewMode(true);
 		m_canvas->setForcedRedraw(true);
 		pm = QImage(clipw, cliph, QImage::Format_ARGB32_Premultiplied);
-		ScPainter *painter = new ScPainter(&pm, pm.width(), pm.height(), 1.0, 0);
+		ScreenPainter *painter = new ScreenPainter(&pm, pm.width(), pm.height(), 1.0, 0);
 		painter->clear(Doc->paperColor());
 		painter->translate(-clipx, -clipy);
 		painter->setLineWidth(1);
@@ -2715,11 +2715,11 @@ QImage ScribusView::PageToPixmap(int Nr, int maxGr, bool drawFrame, bool drawBac
 			Doc->setMasterPageMode(false);
 			Doc->setLoading(true);
 			Doc->setCurrentPage(Doc->DocPages.at(Nr));
-			ScPainter *painter = new ScPainter(&im, im.width(), im.height(), 1.0, 0);
+			ScreenPainter *painter = new ScreenPainter(&im, im.width(), im.height(), 1.0, 0);
 			if (drawBackground)
 				painter->clear(Doc->paperColor());
 			painter->translate(-clipx, -clipy);
-			painter->setFillMode(ScPainter::Solid);
+			painter->setFillMode(ScreenPainter::Solid);
 			if (drawFrame)
 			{
 				painter->setPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);

HOST-Oman/scribus

scribus/scribusview.cpp
@@ -2634,7 +2634,7 @@ QImage ScribusView::MPageToPixmap(QString name, int maxGr, bool drawFrame)
 		m_canvas->setPreviewMode(true);
 		m_canvas->setForcedRedraw(true);
 		pm = QImage(clipw, cliph, QImage::Format_ARGB32_Premultiplied);
-		ScPainter *painter = new ScPainter(&pm, pm.width(), pm.height(), 1.0, 0);
+		ScPainter *painter = new ScScreenPainter(&pm, pm.width(), pm.height(), 1.0, 0);
 		painter->clear(Doc->paperColor());
 		painter->translate(-clipx, -clipy);
 		painter->setLineWidth(1);
@@ -2717,7 +2717,7 @@ QImage ScribusView::PageToPixmap(int Nr, int maxGr, bool drawFrame, bool drawBac
 	Doc->setMasterPageMode(false);
 	Doc->setLoading(true);
 	Doc->setCurrentPage(Doc->DocPages.at(Nr));
-	ScPainter *painter = new ScPainter(&im, im.width(), im.height(), 1.0, 0);
+	ScPainter *painter = new ScScreenPainter(&im, im.width(), im.height(), 1.0, 0);
 	if (drawBackground)
 		painter->clear(Doc->paperColor());
 	painter->translate(-clipx, -clipy);