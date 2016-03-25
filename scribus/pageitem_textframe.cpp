4af4ba8c332b13920a506fc732d8278136bb2571
Khaled Hosny <khaledhosny@eglug.org>
2016-03-26 01:38:42 +0400

    CID 1357149 (#1 of 1): Uninitialized scalar field
    Initialize the variables in the constructor to make Coverity happy
    though they are initialised elsewhere. The code is really bad though, so
    I don’t blame it.

--- scribus/pageitem_textframe.cpp
+++ scribus/pageitem_textframe.cpp
@@ -468,9 +468,6 @@ struct LineControl {
 	double   restartX; //starting X position of line if must be restarted
 	double   rowDesc;
 
-	double   ascend;
-	double   descend;
-	double   width;
 	double   xPos;
 	double   yPos;
 	int      breakIndex;
@@ -494,6 +491,29 @@ struct LineControl {
 		colWidth = colwidth;
 		colGap = colgap;
 		hyphenCount = 0;
+
+		isEmpty = true;
+		colLeft = insets.left() + lineCorr;
+		colRight = colLeft + colWidth;
+		startOfCol = true;
+		afterOverflow = false;
+		addLine = false;
+		recalculateY = false;
+		lastInRowLine = false;
+		addLeftIndent = false;
+		wasFirstInRow = false;
+		leftIndent = 0.0;
+		rightMargin = 0.0;
+		mustLineEnd = false;
+		restartIndex = 0;
+		restartRowIndex = 0;
+		restartX = 0.0;
+		rowDesc = 0.0;
+		xPos = 0.0;
+		yPos = 0.0;
+		breakIndex = -1;
+		breakXPos = 0.0;
+		maxShrink = 0.0;
 	}
 
 	/// move position to next column
@@ -537,7 +557,6 @@ struct LineControl {
 		breakXPos = 0.0;
 		maxShrink = 0.0;
 		maxStretch = 0.0;
-		width = 0.0;
 		leftIndent = 0.0;
 		rightMargin = 0.0;
 		rowDesc = 0.0;
