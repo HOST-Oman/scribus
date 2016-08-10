62e57e2707ac7fd7394cd1f9c25d94a330db13e7
Fahad Al-Saidi <fahad.alsaidi@gmail.com>
2016-08-10 10:14:47 +0400

    adding license header

--- scribus/text/index.cpp
+++ scribus/text/index.cpp
@@ -1,10 +1,8 @@
 /*
- *  index.cpp
- *  ScribusProject
- *
- *  Created by Andreas Vox on 30.11.09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
+ For general Scribus (>=1.3.2) copyright and licensing information please refer
+ to the COPYING file provided with the program. Following this notice may exist
+ a copyright and/or license notice that predates the release of Scribus 1.3.2
+ for which a new license (GPL+exception) is in place.
  */
 
 #include <QDebug>
